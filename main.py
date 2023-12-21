6033956f1fee030b90afa58373a934d394a21fcd
bhavya416 <bhavyapadaliya179@gmail.com>
2023-12-22 00:00:47 +0530

    IN ADD CONTACT CONDITIONS ADDED IF THE SAME NAME OR SAME NUMBER IS ALREADY EXISTS OR NOT

--- main.py
+++ main.py
@@ -2,16 +2,34 @@ print("\n\t\tWelcome\tContact Book") # title
 
 def addcontact(name : str,number : int): # function to add contacts to contacts.csv
     
-    pass
+    file = open("contacts.csv","r") # creating file object to read existing contact details
 
+    all_contacts_data = file.readlines() # read all contacts details 
 
+    for i in all_contacts_data: # iterate through each contact ( each person contact )
+        contact_detail = i.split(",") # split the name and contact number and other details to list to access individual
 
+        if contact_detail[0] == name: # check if the name is already exists or not
+            print("\nName Already Exists , Choose Different Name") # message 
+            print("\nExisting Contact Details :-") 
+            print("Name : " + contact_detail[0] + "    Number : " + contact_detail[1]) # printing the contact details of the same name matched
+            return # return , function call ended ( no further execution of function )
+        
+        
+        elif contact_detail[1] == number: # check if the number is already exists or not
+
+            print("\nNumber Already Exists , Check The Number And Try Again")
+            print("Name : " + contact_detail[0] + "    Number : " + contact_detail[1]) # printing the contact details of the same number matched
+            return # return , function call ended ( no further execution of function )
+        
+        else:
+            pass
 
 # main code starts
 
 while True: # starting of main menu
 
-    print("Main Menu\n") # main menu title
+    print("\nMain Menu\n") # main menu title
 
     print("1 ) Add Contacts") # option for add contact
     print("0 ) Quit") # option to quit app / program
@@ -26,11 +44,3 @@ while True: # starting of main menu
         new_contact_number = input("Enter Number ( With Country Code) eg . +91xxxxxxxxxx : ") # input the number for new contact
 
         addcontact(new_contact_name,new_contact_number)
-
-
-
-
-
-
-
-    
