7bbda8aac61392dc26fee20ae6a226f5a1375b99
bhavya416 <bhavyapadaliya179@gmail.com>
2023-12-21 18:19:13 +0530

    DEFAULT CODE ADDED

---
+++ main.py
@@ -0,0 +1,13 @@
+print("\n\t\tWelcome\tContact Book") # title
+
+def addcontact(name : str,number : int): # function to add contacts to contacts.csv
+    pass
+
+while True: # starting of main menu
+
+    print("Main Menu\n") # main menu title
+
+    print("1 ) Add Contacts") 
+
+    
+    
