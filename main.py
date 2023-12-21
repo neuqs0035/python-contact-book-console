4c9c7cbb292251a01a7da982ea024cf82e2514f2
bhavya416 <bhavyapadaliya179@gmail.com>
2023-12-21 18:36:18 +0530

    INPUTS ADDED FOR ADD CONTACT OPTION AND METHOD CALLING ADDED

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 print("\n\t\tWelcome\tContact Book") # title
 
 def addcontact(name : str,number : int): # function to add contacts to contacts.csv
+    
     pass
+
+
+
+
+# main code starts
 
 while True: # starting of main menu
 
     print("Main Menu\n") # main menu title
 
-    print("1 ) Add Contacts") 
+    print("1 ) Add Contacts") # option for add contact
+    print("0 ) Quit") # option to quit app / program
 
-    
+    input_choice = int(input("_ : "))
+
+    if( input_choice == 1): # if input choice from user is 1 ( to add contact )
+
+        print("\nADD CONTACT") # title for add contact
+
+        new_contact_name = input("\nEnter Name Of Contact : ") # input the name for new contact
+        new_contact_number = input("Enter Number ( With Country Code) eg . +91xxxxxxxxxx : ") # input the number for new contact
+
+        addcontact(new_contact_name,new_contact_number)
+
+
+
+
+
+
+
     
