59ff5ed168bf193697d03095d1baa18e94249dac
bhavya416 <bhavyapadaliya179@gmail.com>
2023-12-22 22:38:58 +0530

    FUNCTION CODE ADDED TO ADD CONTACT DETAILS IF THE NAME AND NUMBERS ARE NOT ALREADY EXISTS

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 print("\n\t\tWelcome\tContact Book") # title
 
-def addcontact(name : str,number : int): # function to add contacts to contacts.csv
-    
+def addcontact(name : str,number : str,contact_type : str): # function to add contacts to contacts.csv
+   
     file = open("contacts.csv","r") # creating file object to read existing contact details
 
     all_contacts_data = file.readlines() # read all contacts details 
+
+    file.close() # close file object to release memory
 
     for i in all_contacts_data: # iterate through each contact ( each person contact )
         contact_detail = i.split(",") # split the name and contact number and other details to list to access individual
@@ -15,15 +17,24 @@ def addcontact(name : str,number : int): # function to add contacts to contacts.
             print("Name : " + contact_detail[0] + "    Number : " + contact_detail[1]) # printing the contact details of the same name matched
             return # return , function call ended ( no further execution of function )
         
-        
         elif contact_detail[1] == number: # check if the number is already exists or not
 
             print("\nNumber Already Exists , Check The Number And Try Again")
             print("Name : " + contact_detail[0] + "    Number : " + contact_detail[1]) # printing the contact details of the same number matched
             return # return , function call ended ( no further execution of function )
-        
-        else:
-            pass
+     
+    # if number and names are not already exists then after for loop add new contact
+    file = open("contacts.csv","a") # create a file object to append data to file contacts.csv
+   
+    new_contact_detail = name + "," + number + "," + contact_type + "\n" # create a complete string for csv file 
+
+    file.write(new_contact_detail) # append data to file
+    
+    file.close() # close the file object to release momory
+
+    print("\nContact Details Added Successfully") # after successfully adding new contact details to file displays message
+    return # return to called
+
 
 # main code starts
 
@@ -34,13 +45,18 @@ while True: # starting of main menu
     print("1 ) Add Contacts") # option for add contact
     print("0 ) Quit") # option to quit app / program
 
-    input_choice = int(input("_ : "))
+    input_choice = int(input("\n_ : "))
 
     if( input_choice == 1): # if input choice from user is 1 ( to add contact )
 
         print("\nADD CONTACT") # title for add contact
 
         new_contact_name = input("\nEnter Name Of Contact : ") # input the name for new contact
-        new_contact_number = input("Enter Number ( With Country Code) eg . +91xxxxxxxxxx : ") # input the number for new contact
+        new_contact_number = input("Enter Number ( With Country Code) eg . +91xxx : ") # input the number for new contact
 
-        addcontact(new_contact_name,new_contact_number)
+        addcontact(new_contact_name,new_contact_number,"normal")
+
+    elif input_choice == 0: # if input choice from user is 0 ( to exit program )
+
+        print("\nProgram Exited -\n") # display message
+        break # break from main while loop to exit main menu
